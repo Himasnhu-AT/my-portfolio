9f8d4fb8ca56af7767b236933a2e0f79a360c1e1
Himanshu-AT <aristech.official@gmail.com>
2024-02-26 23:50:12 +0530

    Add skills section to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,31 +3,67 @@ import { StickyScroll } from "@/components/stickyScroll";
 import { Scroll } from "lucide-react";
 import React from "react";
 
+const Skills = () => {
+  return (
+    <div className="max-w-4xl mx-auto">
+      <h2 className="text-3xl font-bold mb-4">Programming Languages</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <div className=" ">JavaScript</div>
+        <div className=" ">TypeScript</div>
+        <div className=" ">Rust</div>
+        <div className=" ">Python</div>
+        <div className=" ">Kotlin</div>
+      </div>
+
+      <h2 className="text-3xl font-bold my-4">Frameworks & Libraries</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <div className=" ">React</div>
+        <div className=" ">Next.js</div>
+        <div className=" ">Node.js</div>
+        <div className=" ">Nest.js</div>
+        <div className=" ">Prisma</div>
+        <div className=" ">Jest</div>
+        <div className=" ">TailwindCSS</div>
+        <div className=" ">Tensorflow</div>
+      </div>
+
+      <h2 className="text-3xl font-bold my-4">Developer Tools</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <div className=" ">Git</div>
+        <div className=" ">Docker</div>
+        <div className=" ">VS Code</div>
+        <div className=" ">Google Cloud</div>
+        <div className=" ">Azure</div>
+        <div className=" ">Linux Server</div>
+        <div className=" ">SSH</div>
+      </div>
+
+      <h2 className="text-3xl font-bold my-4">Libraries</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <div className=" ">SciKit-learn</div>
+        <div className=" ">Numpy</div>
+        <div className=" ">Pandas</div>
+        <div className=" ">Matplotlib</div>
+      </div>
+    </div>
+  );
+};
+
 const content = [
   {
     title: "About Me",
     description:
-      "Hi there! I'm a software engineer and this is my portfolio. I'm passionate about building products that make a difference. I'm always looking for opportunities to work on exciting projects with amazing people. If you're interested in collaborating, feel free to reach out to me.",
+      "Passionate software engineer with a track record of crafting impactful products. I thrive on collaborative projects and enjoy working with talented individuals. Let's connect and explore opportunities to create something remarkable together.",
     image: "/profileImage.png",
   },
   {
-    title: "Collaborative Editing",
+    title: "Open Source Contributions",
     description:
-      "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
+      "I'm a strong advocate for open source and regularly contribute to various projects. Check out some of my contributions. I'm always looking for new projects to contribute to. If you have a project that you think I might be interested in, feel free to reach out.",
   },
   {
-    title: "Real time changes",
-    description:
-      "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
-  },
-  {
-    title: "Version control",
-    description:
-      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
-  },
-  {
-    title: "My Skills",
-    description: <Scroll size={30} />,
+    title: "Skills",
+    description: <Skills />,
   },
 ];
 
