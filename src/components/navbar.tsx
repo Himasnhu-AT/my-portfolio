c18174ff4f6f9da48ceb0a0037298427d1be7bfd
Himanshu-AT <aristech.official@gmail.com>
2024-02-26 22:12:51 +0530

    Add new files and update dependencies

---
+++ src/components/navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className=" text-white">
+      <h1>My Portfolio</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
